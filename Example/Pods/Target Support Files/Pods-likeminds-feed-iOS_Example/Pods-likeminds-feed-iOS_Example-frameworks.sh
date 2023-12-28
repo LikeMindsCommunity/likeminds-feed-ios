18fba2752b6964ba923799f77e51f37e326b0e11
Devansh Mohata <devansh.mohata@likeminds.community>
2023-12-28 13:42:57 +0530

    Added LikeMindsFeed SDK

--- Example/Pods/Target Support Files/Pods-likeminds-feed-iOS_Example/Pods-likeminds-feed-iOS_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-likeminds-feed-iOS_Example/Pods-likeminds-feed-iOS_Example-frameworks.sh
@@ -176,12 +176,16 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/likeminds-feed-iOS/likeminds_feed_iOS.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/LikeMindsFeed/LikeMindsFeed.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/likeminds-feed-iOS/likeminds_feed_iOS.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/LikeMindsFeed/LikeMindsFeed.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
