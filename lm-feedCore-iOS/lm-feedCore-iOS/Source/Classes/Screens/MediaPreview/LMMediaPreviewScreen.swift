d910652ef478e675ab36361a6a820595420c2744
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2024-07-24 10:22:33 +0530

    init: added media preview screen and viewmodel

---
+++ lm-feedCore-iOS/lm-feedCore-iOS/Source/Classes/Screens/MediaPreview/LMMediaPreviewScreen.swift
@@ -0,0 +1,13 @@
+//
+//  MediaPreviewScreenViewController.swift
+//  LikeMindsFeedCore
+//
+//  Created by Anurag Tyagi on 24/07/24.
+//
+
+import Foundation
+import LikeMindsFeedUI
+
+class LMMediaPreviewScreen: LMViewController{
+    
+}
---
+++ lm-feedCore-iOS/lm-feedCore-iOS/Source/Classes/Screens/MediaPreview/LMMediaPreviewViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  LMMediaPreviewViewModel.swift
+//  LikeMindsFeedCore
+//
+//  Created by Anurag Tyagi on 24/07/24.
+//
+
+import Foundation
+
+import LikeMindsFeedUI
+import LikeMindsFeed
+
+// MARK: LMUniversalFeedViewModelProtocol
+public protocol LMMediaPreviewViewModelProtocol: LMBaseViewControllerProtocol {
+    
+}
+
+public class LMMediaPreviewViewModel {
+    
+}
